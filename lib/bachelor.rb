bfcfb2d351fe2c2bca4e4ed503793c6e78125dd3
aeremin <aeremin@maxus.ru>
2015-06-16 13:39:52 +0300

    done

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,36 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].detect { |hsh| hsh["status"] == "Winner" }["name"].split[0]
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.reduce("") do |name, (season, contestants)|
+    obj = contestants.detect { |cont| cont["occupation"] == occupation }
+	obj == nil ? name : obj["name"]
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  data.reduce(0) do |sum, (season, contestants)|
+    sum + contestants.select { |cont| cont["hometown"] == hometown }.size
+  end
 end
 
 def get_occupation(data, hometown)
   # code here
+  data.reduce("") do |occupation, (season, contestants)|
+    obj = contestants.detect { |cont| cont["hometown"] == hometown }
+	obj == nil ? occupation : obj["occupation"]
+  end
 end
 
 def get_average_age_for_season(data, season)
   # code here
+  result_hash = data[season].each_with_object({ :sum => 0.0, :num => 0 }) do |cont, hsh|
+    hsh[:sum] += cont["age"].to_f
+	hsh[:num] += 1
+  end
+  (result_hash[:sum]/result_hash[:num]).round
 end
--- spec/bachelor_spec.rb
+++ spec/bachelor_spec.rb
@@ -7,11 +7,11 @@ describe "bachelor" do
       expect(get_first_name_of_season_winner(data, "season 10")).to eq("Tessa")
     end
 
-    it "returns 'Courtney' when passed the data and 'season 10'" do
+    it "returns 'Courtney' when passed the data and 'season 16'" do
       expect(get_first_name_of_season_winner(data, "season 16")).to eq("Courtney")
     end
 
-    it "returns 'Emily' when passed the data and 'season 10'" do
+    it "returns 'Emily' when passed the data and 'season 15'" do
       expect(get_first_name_of_season_winner(data, "season 15")).to eq("Emily")
     end
   end
@@ -62,7 +62,7 @@ describe "bachelor" do
     it "returns 26 when passed the data and the string 'season 10'" do
       expect(get_average_age_for_season(data, "season 10")).to eq(26)
     end
-    it "returns 26 when passed the data and the string 'season 11'" do
+    it "returns 27 when passed the data and the string 'season 11'" do
       expect(get_average_age_for_season(data, "season 11")).to eq(27)
     end
     it "returns 26 when passed the data and the string 'season 12'" do
