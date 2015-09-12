44c4c62ffa18fb516bfd1554ec0a62e3b19a5701
Rsenger <Rgermosen12@gmail.com>
2015-09-12 08:26:18 -0400

    bachelors

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,57 @@
+require 'pry'
+
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+	data.each do |seasons, grouped_info|
+		grouped_info.each do |info, answer|
+			if season == seasons && info["status"] == "Winner"
+				first_name = info["name"].split(' ')[0]
+				return first_name
+			end
+		end
+	end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  	data.each do |seasons, grouped_info|
+  		grouped_info.each do |info, answer|
+  			if occupation == info["occupation"]
+  				return info["name"]
+  			end
+  		end
+  	end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  location = 0
+  data.each do |seasons, grouped_info|
+  	grouped_info.each do |info, answer|
+  		if info["hometown"] == hometown
+  			location += 1 
+ 	   	end
+  	end
+  end
+  location
 end
 
 def get_occupation(data, hometown)
-  # code here
+  	data.each do |seasons, grouped_info|
+  		grouped_info.each do |info, answer|
+  			if hometown == info["hometown"]
+  				return info["occupation"]
+  			end
+  		end
+  	end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+	ages = []
+  	data.each do |seasons, grouped_info|
+  		grouped_info.each do |info, answer|
+  			if season == seasons
+  				ages << info["age"].to_i
+  			end
+  		end
+  	end
+  	return ages.inject(:+)/ages.length
 end
